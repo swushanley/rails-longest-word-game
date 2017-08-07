095b99a51c9cfd55494e7ab0cb37a9cbb29db71f
Sophia Wushanley <swushanley@gmail.com>
2017-08-07 17:37:08 +0200

    finished all of longest word game except instances in word_game_controller.rb and corresponding stuff in score.html.erb

---
+++ app/controllers/word_game_controller.rb
@@ -0,0 +1,63 @@
+class WordGameController < ApplicationController
+  def game
+    @grid = generate_grid(rand(5..10))
+
+    # @compute_score = compute_score(attempt, time_taken)
+    # @run_game = run_game(attempt, grid, start_time, end_time)
+  end
+
+  def score
+    @query = params[:query]
+    @grid = params[:grid]
+    @included = included?(@query, @grid)
+    @start_time = Time.parse(params[:start_time])
+    @end_time = Time.now
+    @time_taken = @end_time - @start_time
+    @compute_score = compute_score(@query, @time_taken)
+    @run_game = run_game(@query, @grid, @start_time, @end_time)
+    @english_word = english_word?(@query)
+    @score_and_message = score_and_message(@query, @grid, @time_taken)
+
+  end
+end
+
+def generate_grid(grid_size)
+  Array.new(grid_size) { ('A'..'Z').to_a.sample }
+end
+
+def included?(guess, grid)
+  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+end
+
+def compute_score(attempt, time_taken)
+  time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+end
+
+def run_game(attempt, grid, start_time, end_time)
+  result = { time: end_time - start_time }
+
+  score_and_message = score_and_message(attempt, grid, result[:time])
+  result[:score] = score_and_message.first
+  result[:message] = score_and_message.last
+
+  result
+end
+
+def score_and_message(attempt, grid, time)
+  if included?(attempt.upcase, grid)
+    if english_word?(attempt)
+      score = compute_score(attempt, time)
+      [score, "well done"]
+    else
+      [0, "not an english word"]
+    end
+  else
+    [0, "not in the grid"]
+  end
+end
+
+def english_word?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  return json['found']
+end
